a64968f92eb9b6b6c02c76acfdde8db4f1aa0003
Yuhan Liu <palast.liu@gmail.com>
2026-04-20 22:38:26 +0800

    add project structure and initial files

---
+++ test.py
@@ -0,0 +1,13 @@
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+import sklearn
+import cv2
+from PIL import Image
+
+print("TensorFlow:", tf.__version__)
+print("NumPy:", np.__version__)
+print("Pandas:", pd.__version__)
+print("scikit-learn:", sklearn.__version__)
+print("OpenCV:", cv2.__version__)
+print("Pillow: OK")
